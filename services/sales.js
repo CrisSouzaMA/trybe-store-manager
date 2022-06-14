9ee0ba72035caa30202f3425f769369ca2d66916
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-14 18:04:21 -0300

    Feat: implementa regra de negocio para pegar venda pelo id

--- services/sales.js
+++ services/sales.js
@@ -2,6 +2,15 @@ const saleModel = require('../models/sales');
 
 const getAll = async () => await saleModel.getAll();
 
+const getById = async (id) => {
+  const saleId = await saleModel.getById(id);
+
+  if (!saleId) throw new Error('Sale not found');
+
+  return saleId;
+}
+
 module.exports = {
   getAll,
+  getById,
 }
