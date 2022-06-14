10822cf37d4324f8590904da54533dd6ca5eb8c9
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-14 17:26:22 -0300

    Feat: implementa regra de negocio da busca de produto pelo id

--- services/products.js
+++ services/products.js
@@ -2,6 +2,15 @@ const productModel = require('../models/products');
 
 const getAll = async () => await productModel.getAll();
 
+const getById = async (id) => {
+  const productId = await productModel.getById(id);
+  
+  if (!productId || productId === undefined) throw new Error('Product not found');
+
+  return productId;
+}
+
 module.exports = {
   getAll,
+  getById,
 }
