789a7db628b650502b7ac688ebecf3f966a4d29f
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-14 17:27:06 -0300

    Feat: implementa controller para requisição de produto pelo numero do id vindo pela URL

--- controllers/products.js
+++ controllers/products.js
@@ -6,6 +6,17 @@ const getAll = async (_req, res) => {
   res.status(200).json(allproducts);
 }
 
+const getById = async (req, res) => {
+  try {
+  const { id } = req.params;
+  const productId = await productService.getById(id);
+  return res.status(200).json(productId);
+  } catch (e) {
+    return res.status(404).end()
+  }
+}
+
 module.exports = {
   getAll,
+  getById,
 }
