d620c5eccd3191c45dc9693602f84ea53295fb5d
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-14 16:37:14 -0300

    FEat: chama rota de produtos no index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+const express = require('express');
 require('dotenv').config();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Escutando na porta ${process.env.PORT}`);
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+const productRouter = require('./routes/products');
+
+app.use('/products', productRouter);
+
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
