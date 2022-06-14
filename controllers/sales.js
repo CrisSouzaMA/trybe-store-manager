ef5ff6294af18b542b4f13e80113ba0553a34e29
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-14 18:05:23 -0300

    Feat: implementa requisição para pegar venda pelo id

--- controllers/sales.js
+++ controllers/sales.js
@@ -1,4 +1,3 @@
-const res = require('express/lib/response');
 const saleService = require('../services/sales');
 
 const getAll = async (_req, res) => {
@@ -6,6 +5,17 @@ const getAll = async (_req, res) => {
   res.status(200).json(allSales);
 }
 
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const saleId = await saleService.getById(Number(id));
+    res.status(200).json(saleId);
+  } catch (e) {
+    return res.status(404).json({ message: e.message });
+  }
+}
+
 module.exports = {
   getAll,
+  getById,
 }
