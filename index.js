4a50e42ad5f3c6a4b10946ba2317a8f9270a0b92
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-14 17:51:15 -0300

    Feat: implementa rota de sales no index.js

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 const productRouter = require('./routes/products');
+const saleRouter = require('./routes/sales');
 
 app.use('/products', productRouter);
+app.use('/sales', saleRouter);
 
 app.listen(PORT, () => {
   console.log(`Escutando na porta ${PORT}`);
