43b4b67efd3663b0ac59a8b64cc3a1247ff1a79a
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-16 18:57:10 -0300

    Feat: implementa regra de negocio para criação de nova venda

--- services/sales.js
+++ services/sales.js
@@ -10,7 +10,21 @@ const getById = async (id) => {
   return saleId;
 }
 
+const create = async (arrayOfSales) => {
+  await Promise.all(arrayOfSales.map(async (data) => {
+    const productReturn = await ProductModel.getById(data.productId);
+    if (productReturn.quantity < data.quantity) {
+      throw new Error('Such amount is not permitted to sell');
+    }
+    productReturn.quantity -= data.quantity;
+    await ProductModel.update(productReturn);
+  }));
+  const createSales = await saleModel.create(arrayOfSales);
+  return createSales;
+};
+
 module.exports = {
   getAll,
   getById,
+  create,
 }
