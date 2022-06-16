375fda8ef07a2b8a14947eff6649bed3e09bdb26
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-16 19:03:13 -0300

    Feat: implementa validação na rota de criação

--- routes/sales.js
+++ routes/sales.js
@@ -4,8 +4,12 @@ const router = express.Router();
 
 const saleController = require('../controllers/sales');
 
+const saleMiddleware = require('../middlewares/validationSale');
+
 router.get('/', saleController.getAll);
 router.get('/:id', saleController.getById);
+
+router.use(saleMiddleware.validateSale);
 
 router.post('/', saleController.create);
 
