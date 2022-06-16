6541ef48210f61b888519f0a8ace2224f117f62c
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-16 17:23:53 -0300

    Feat: implementa rota post para /products

--- routes/products.js
+++ routes/products.js
@@ -4,7 +4,13 @@ const router = express.Router();
 
 const productController = require('../controllers/products');
 
+const productMiddleware = require('../middlewares/validationProduct');
+
 router.get('/', productController.getAll);
 router.get('/:id', productController.getById);
+
+router.use(productMiddleware.validateProduct);
+
+router.post('/', productController.create);
 
 module.exports = router;
