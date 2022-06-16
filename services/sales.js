9c9a988bef2a47601b5f35ad022726b95a3a470c
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-16 19:23:21 -0300

    Feat: implementa regra de negócio para deletar uma venda

--- services/sales.js
+++ services/sales.js
@@ -31,9 +31,27 @@ const update = async ({ id, arrayOfSales }) => {
   return upSale;
 }
 
+const delet = async (id) => {
+  try {
+    const checkId = await saleModel.findBySaleId(id);
+    const checkSale = await saleModel.getById(id);
+    await Promise.all(checkSale.map(async (data) => {
+      const productReturn = await ProductModel.getById(data.productId);
+      productReturn.quantity += data.quantity;
+      await ProductModel.update(productReturn);
+    }));
+    if (checkId.length !== 0) {
+      await saleModel.deleteSale(id);
+    }
+  } catch (error) {
+    throw new Error('Sale not found');
+  }
+}
+
 module.exports = {
   getAll,
   getById,
   create,
   update,
+  delet,
 }
