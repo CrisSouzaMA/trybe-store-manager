3d18510a3a1c32cb587fdcfe75521c0e811775cd
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-16 17:25:17 -0300

    Fix: ajeita query de sales pois tabela possui tabela de junção com produtos

--- models/sales.js
+++ models/sales.js
@@ -1,14 +1,20 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const query = 'SELECT * FROM StoreManager.sales';
+  const query =  `SELECT s.date, p.sale_id AS saleId, p.product_id AS productId, p.quantity
+  FROM StoreManager.sales s INNER JOIN StoreManager.sales_products p 
+  ON s.id = p.sale_id
+  ORDER BY p.sale_id`;
   const [result] = await connection.execute(query);
 
   return result;
 }
 
 const getById = async (id) => {
-  const query = 'SELECT * FROM StoreManager.sales WHERE id=?';
+  const query = `SELECT s.date, p.product_id, p.quantity
+  FROM StoreManager.sales s INNER JOIN StoreManager.sales_products p
+  ON s.id = p.sale_id 
+  WHERE s.id=?`;
   const [result] = await connection.execute(query, [id]);
 
   return result;
