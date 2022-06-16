08d70c5aeaeedf07d6a1bf3bb0da31830a44be03
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2022-06-16 17:22:21 -0300

    Feat: implementa query para criação de enovo produto

--- models/products.js
+++ models/products.js
@@ -11,10 +11,30 @@ const getById = async (id) => {
   const query = 'SELECT * FROM StoreManager.products WHERE id =?';
   const [result] = await connection.execute(query, [id]);
 
-  return result;
+  return result.map(({ id, name, quantity }) => ({
+    id,
+    name,
+    quantity,
+  }))[0];
+}
+
+const getByName = async (name) => {
+  const query = 'SELECT id, name, quantity FROM StoreManager.products WHERE name=?';
+  const [product] = await connection.execute(query, [name]);
+
+  return product;
+}
+
+const create = async ({ name, quantity }) => {
+  const query = 'INSERT INTO StoreManager.products (name, quantity) VALUES (?,?);'
+  const [newproduct] = await connection.execute(query, [name, quantity]);
+
+  return { id: newproduct.insertId, name, quantity };
 }
 
 module.exports = {
   getAll,
   getById,
+  create,
+  getByName,
 }
